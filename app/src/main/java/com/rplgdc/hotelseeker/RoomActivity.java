507035a47cb15b1dc216f2a3e57c57e6ed3bfae7
Ardityo Cahyo <ardityoc@gmail.com>
2020-07-09 22:08:35 +0700

    Change the 'Rooms and Guests' Layout and onClickListener

--- app/src/main/java/com/rplgdc/hotelseeker/RoomActivity.java
+++ app/src/main/java/com/rplgdc/hotelseeker/RoomActivity.java
@@ -1,8 +1,12 @@
 package com.rplgdc.hotelseeker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.drawable.DrawableCompat;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,8 +23,11 @@ public class RoomActivity extends AppCompatActivity {
     private LinearLayout sumLayout;
     private Button roomPlus;
     private Button roomMin;
+    private Button guestPlus;
+    private Button guestMin;
 
     public int sumRoom;
+    public int sumGuest;
 
 
     @Override
@@ -28,22 +35,36 @@ public class RoomActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room);
 
-        sumLayout = (LinearLayout) findViewById(R.id.sum);
+//        sumLayout = (LinearLayout) findViewById(R.id.sum);
 
         setTitle("Rooms and Guests");
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        final TextView textSum = (TextView) findViewById(R.id.sumRooms);
-        sumRoom = Integer.parseInt(textSum.getText().toString());
+        final TextView textSumRoom = (TextView) findViewById(R.id.sumRooms);
+        sumRoom = Integer.parseInt(textSumRoom.getText().toString());
+
+        final TextView textSumGuest = (TextView) findViewById(R.id.sumGuests);
+        sumGuest = Integer.parseInt(textSumGuest.getText().toString());
 
         if (sumRoom == min){
             roomMin = (Button) findViewById(R.id.roomMin);
             roomMin.setEnabled(false);
-            
+            guestMin = (Button) findViewById(R.id.guestsMin);
+            guestMin.setEnabled(false);
         }
 
         roomPlus = (Button) findViewById(R.id.roomPlus);
+
+        TypedValue typedValueRoom = new TypedValue();
+        getTheme().resolveAttribute(R.attr.colorPrimary, typedValueRoom, true);
+        final int colorPrimary = typedValueRoom.data;
+
+        Drawable buttonDrawableRoom = roomPlus.getBackground();
+        buttonDrawableRoom = DrawableCompat.wrap(buttonDrawableRoom);
+        DrawableCompat.setTint(buttonDrawableRoom, colorPrimary);
+        roomPlus.setBackground(buttonDrawableRoom);
+
         roomPlus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -51,22 +72,27 @@ public class RoomActivity extends AppCompatActivity {
                 if (sumRoom < max){
                     for(int i = 1; i <= sumRoom; i++){
 //                        LinearLayout roomCard = (LinearLayout) LayoutInflater.from(RoomActivity.this).inflate(R.layout.content_card_room, null);
+//                        roomCard.removeAllViews();
 //                        view = (View) LayoutInflater.from(RoomActivity.this).inflate(R.layout.content_card_room, null);
 //                        TextView roomNumber = (TextView) findViewById(R.id.roomNum);
 //
 //                        roomNumber.setText("Room " + i);
 //                        sumLayout.addView(roomCard);
 
-                        Toast.makeText(RoomActivity.this, "Kurang +"+ i , Toast.LENGTH_SHORT).show();
+//                        Toast.makeText(RoomActivity.this, "Kurang +"+ i , Toast.LENGTH_SHORT).show();
                     }
                     roomPlus.setEnabled(true);
                     roomMin.setEnabled(true);
                 }else{
                     sumRoom = max;
                     roomPlus.setEnabled(false);
-                    Toast.makeText(RoomActivity.this, "Lebih +", Toast.LENGTH_SHORT).show();
+                    Drawable buttonDrawableRoom = roomPlus.getBackground();
+                    buttonDrawableRoom = DrawableCompat.wrap(buttonDrawableRoom);
+                    DrawableCompat.setTint(buttonDrawableRoom, Color.LTGRAY);
+                    roomPlus.setBackground(buttonDrawableRoom);
+//                    Toast.makeText(RoomActivity.this, "Lebih +", Toast.LENGTH_SHORT).show();
                 }
-                textSum.setText(String.valueOf(sumRoom));
+                textSumRoom.setText(String.valueOf(sumRoom));
             }
         });
 
@@ -75,18 +101,84 @@ public class RoomActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 sumRoom -= 1;
+                Drawable buttonDrawable = roomPlus.getBackground();
+                buttonDrawable = DrawableCompat.wrap(buttonDrawable);
+                DrawableCompat.setTint(buttonDrawable, colorPrimary);
+                roomPlus.setBackground(buttonDrawable);
                 if (sumRoom > min){
                     for(int i = 1; i < sumRoom; i++){
-                        Toast.makeText(RoomActivity.this, "Kurang -", Toast.LENGTH_SHORT).show();
+//                        Toast.makeText(RoomActivity.this, "Kurang -", Toast.LENGTH_SHORT).show();
                     }
                     roomPlus.setEnabled(true);
                     roomMin.setEnabled(true);
                 }else{
                     sumRoom = min;
                     roomMin.setEnabled(false);
-                    Toast.makeText(RoomActivity.this, "Lebih -", Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(RoomActivity.this, "Lebih -", Toast.LENGTH_SHORT).show();
                 }
-                textSum.setText(String.valueOf(sumRoom));
+                textSumRoom.setText(String.valueOf(sumRoom));
+            }
+        });
+
+        guestPlus = (Button) findViewById(R.id.guestsPlus);
+
+        Drawable buttonDrawableGuests = guestPlus.getBackground();
+        buttonDrawableGuests = DrawableCompat.wrap(buttonDrawableGuests);
+        DrawableCompat.setTint(buttonDrawableGuests, colorPrimary);
+        guestPlus.setBackground(buttonDrawableGuests);
+
+        guestPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sumGuest += 1;
+                if (sumGuest < max){
+                    for(int i = 1; i <= sumGuest; i++){
+//                        LinearLayout roomCard = (LinearLayout) LayoutInflater.from(RoomActivity.this).inflate(R.layout.content_card_room, null);
+//                        roomCard.removeAllViews();
+//                        view = (View) LayoutInflater.from(RoomActivity.this).inflate(R.layout.content_card_room, null);
+//                        TextView roomNumber = (TextView) findViewById(R.id.roomNum);
+//
+//                        roomNumber.setText("Room " + i);
+//                        sumLayout.addView(roomCard);
+
+//                        Toast.makeText(RoomActivity.this, "Kurang +"+ i , Toast.LENGTH_SHORT).show();
+                    }
+                    guestPlus.setEnabled(true);
+                    guestMin.setEnabled(true);
+                }else{
+                    sumGuest = max;
+                    guestPlus.setEnabled(false);
+                    Drawable buttonDrawableGuests = guestPlus.getBackground();
+                    buttonDrawableGuests = DrawableCompat.wrap(buttonDrawableGuests);
+                    DrawableCompat.setTint(buttonDrawableGuests, Color.LTGRAY);
+                    guestPlus.setBackground(buttonDrawableGuests);
+//                    Toast.makeText(RoomActivity.this, "Lebih +", Toast.LENGTH_SHORT).show();
+                }
+                textSumGuest.setText(String.valueOf(sumGuest));
+            }
+        });
+
+        guestMin = (Button) findViewById(R.id.guestsMin);
+        guestMin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sumGuest -= 1;
+                Drawable buttonDrawableGuests = guestPlus.getBackground();
+                buttonDrawableGuests = DrawableCompat.wrap(buttonDrawableGuests);
+                DrawableCompat.setTint(buttonDrawableGuests, colorPrimary);
+                guestPlus.setBackground(buttonDrawableGuests);
+                if (sumGuest > min){
+                    for(int i = 1; i < sumRoom; i++){
+//                        Toast.makeText(RoomActivity.this, "Kurang -", Toast.LENGTH_SHORT).show();
+                    }
+                    guestPlus.setEnabled(true);
+                    guestMin.setEnabled(true);
+                }else{
+                    sumGuest = min;
+                    guestMin.setEnabled(false);
+//                    Toast.makeText(RoomActivity.this, "Lebih -", Toast.LENGTH_SHORT).show();
+                }
+                textSumGuest.setText(String.valueOf(sumGuest));
             }
         });
     }
