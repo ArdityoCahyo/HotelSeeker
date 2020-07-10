cd356c0ea5d3f28b558a3178534dab18944aa7ab
Ardityo Cahyo <ardityoc@gmail.com>
2020-07-10 23:47:58 +0700

    Add New 'Empty Activity' to the Project

---
+++ app/src/main/java/com/rplgdc/hotelseeker/OnBoardActivity.java
@@ -0,0 +1,14 @@
+package com.rplgdc.hotelseeker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OnBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_board);
+    }
+}
